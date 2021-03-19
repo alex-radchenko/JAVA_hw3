8f65db602ee79f1a2fb0ba51259d53d102810f25
Alex-temp-tech <6504395845.ale.x@gmail.com>
2021-03-18 17:19:23 -0700

    my HW3

---
+++ src/test/java/hw/hw3/hw3.java
@@ -0,0 +1,33 @@
+package hw.hw3;
+import static com.codeborne.selenide.Selectors.byText;
+import com.codeborne.selenide.Configuration;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class hw3 {
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.headless = false;
+    }
+    @Test
+    void Github() {
+        open("https://github.com/");
+        $("input[placeholder='Search GitHub']").val("Selenide").pressEnter();
+        $x("//a[@href='/selenide/selenide']").click();
+        $x("//span[@data-content='Wiki']").click();
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("JUnit5"), text("@ExtendWith({SoftAssertsExtension.class})"));
+    }
+
+    @Test
+    void dragAndDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
